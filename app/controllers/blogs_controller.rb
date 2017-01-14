ea666bd921f58e6816b30f39f28dfafe9d51226f
nishina <n.toshi.1.014@gmail.com>
2017-01-14 21:54:43 +0900

    :sparkles: Add create method

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,49 @@
 # class
 class BlogsController < ApplicationController
   before_action :set_entry, only: [:show]
+
+  # GET /blogs
   def index
     @blogs = Blog.all
   end
 
+  # GET /blogs/1
   def show
   end
 
+  # GET /blog/new
   def new
     @blog = Blog.new
   end
 
+  # def edit
+  #
+  # end
+
+  # POST /blogs
+  def create
+    @blog = Blog.new(blog_params)
+
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # DELETE /entries/1
+  # def destroy
+  #
+  # end
+
   private
     def set_entry
       @blog = Blog.find(params[:id])
+    end
+    def blog_params
+      params.require(:blog).permit(:title)
     end
 end
