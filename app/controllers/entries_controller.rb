61112373452dc01ccdf36cfa740c43043b1d87bc
nishina <n.toshi.1.014@gmail.com>
2017-01-15 00:33:17 +0900

    :sparkles: Add entry form page

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,4 +3,12 @@ class EntriesController < ApplicationController
   #   @blog = Blog.where(:id => params[:blog_id]).first
   #   @entries = @blog.entries.all
   # end
+  def new
+    @blog = Blog.where(:id => params[:blog_id]).first
+    @entry = @blog.entries.build
+  end
+
+  def create
+
+  end
 end
