d64a3a5297cd3e3c4570cebc8a0b6b48ce15174a
nishina <n.toshi.1.014@gmail.com>
2017-01-15 01:07:34 +0900

    :+1: Add redirect for creating entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,14 +1,37 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: [:show]
   # def index
   #   @blog = Blog.where(:id => params[:blog_id]).first
   #   @entries = @blog.entries.all
   # end
+  def show
+  end
+
   def new
     @blog = Blog.where(:id => params[:blog_id]).first
     @entry = @blog.entries.build
   end
 
   def create
+    @blog = Blog.where(:id => params[:blog_id]).first
+    @entry = @blog.entries.build(entry_params)
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to [@blog, @entry] }
+      else
+        format.html { render :new }
+      end
+    end
+
 
   end
+  private
+    def set_entry
+      @blog = Blog.where(:id => params[:blog_id]).first
+      @entry = @blog.entries.find(params[:id])
+    end
+    def entry_params
+      params.require(:entry).permit(:title, :body)
+    end
 end
