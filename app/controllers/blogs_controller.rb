6ac5beb593d07f99c99d776657a54707483a2f69
nishina <n.toshi.1.014@gmail.com>
2017-01-14 18:25:36 +0900

    :sparkles: Add show page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,15 @@
 # class
 class BlogsController < ApplicationController
+  before_action :set_entry, only: [:show]
   def index
     @blogs = Blog.all
   end
+
+  def show
+  end
+
+  private
+    def set_entry
+      @blog = Blog.find(params[:id])
+    end
 end
