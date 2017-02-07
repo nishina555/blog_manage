4493b6eefc04a9952bde7ccc8f77ffaf0fab37a3
nishina <n.toshi.1.014@gmail.com>
2017-02-07 22:39:31 +0900

    :art: Update ui

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,7 +19,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to blog_entry_url(:blog_id => params[:blog_id], :id => params[:entry_id])}
       else
-        format.html { render blog_entry_url(:blog_id => params[:blog_id], :id => params[:entry_id])}
+        format.html { redirect_to blog_entry_url(:blog_id => params[:blog_id], :id => params[:entry_id])}
       end
     end
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :entry
+  validates :body, presence: true
 end
