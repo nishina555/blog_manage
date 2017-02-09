7146c05c5950268440c2756e663ef43e88d39e49
nishina <n.toshi.1.014@gmail.com>
2017-02-09 23:21:14 +0900

    :white_check_mark: Fix test code, fix error message

--- spec/feature/blogs_spec.rb
+++ spec/feature/blogs_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 feature 'Blog管理'do
   scenario 'Blogの新規作成時にtitleを入力した場合はデータが保存され新規ブログの詳細ページに遷移すること' do
-    visit blogs_path
+    visit root_path
     click_link 'New Blog'
     fill_in 'Title', with: 'title'
     expect {
       click_button 'Save'
     }.to change(Blog, :count).by(1)
-    expect(current_path).to eq blog_path(id: Blog.last)
+    expect(current_path).to eq root_path
     expect(page).to have_content 'Blog was successfully created.'
   end
   scenario 'Blogの新規作成時にtitleを入力しなかった場合、新規作成画面に遷移すること' do
-    visit blogs_path
+    visit root_path
     click_link 'New Blog'
     click_button 'Save'
     expect(current_path).to eq blogs_path
-    expect(page).to have_content "Title can't be blank"
+    expect(page).to have_content "can't be blank"
   end
 
 end
